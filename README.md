b8f9b6de26d818b954ea7415daeb67b82f67a05a
laithisbaitan <80163581+laithisbaitan@users.noreply.github.com>
2023-11-03 19:22:21 +0200

    finished backend for blockchain

--- README.md
+++ README.md
@@ -1,17 +1,33 @@
-# PythonBlockchain
- 
 **Activate the virtual environment**
-'''
-source blockchain-env\Scripts\activate
-'''
+```
+source blockchain-env/bin/activate
+```
 
-***Install all packages***
-'''
+**Install all packages**
+```
 pip3 install -r requirements.txt
-'''
+```
 
-***Run the application and API***
-Make sure to activate virtual enviroment.
-'''
+**Run the tests**
+
+Make sure to activate the virtual environment.
+
+```
+python3 -m pytest backend/tests
+```
+
+**Run the application and API**
+
+Make sure to activate the virtual environment.
+
+```
 python3 -m backend.app
-'''
+```
+
+**Run a peer instance**
+
+Make sure to activate the virtual environment.
+
+```
+export PEER=True && python3 -m backend.app
+```
--- backend/pubsub.py
+++ backend/pubsub.py
@@ -1,47 +1,65 @@
 import time
+
 from pubnub.pubnub import PubNub
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
-import uuid
 
-# Generate a random UUID
-random_uuid = str(uuid.uuid4())
+from backend.blockchain.block import Block
 
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = 'sub-c-b0bd4f95-7606-40d6-a6ef-57ad3d5d0f58'
 pnconfig.publish_key = 'pub-c-af608ad6-3ff9-4ad8-8ac5-34043940911b'
-pnconfig.uuid = random_uuid
-
-TEST_CHANNEL = 'TEST_CHANNEL'
-
+CHANNELS = {
+    'TEST': 'TEST',
+    'BLOCK': 'BLOCK'
+}
 
 class Listener(SubscribeCallback):
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
+
     def message(self, pubnub, message_object):
         print(f'\n-- Channel: {message_object.channel} | Message: {message_object.message}')
 
+        if message_object.channel == CHANNELS['BLOCK']:
+            block = Block.from_json(message_object.message)
+            potential_chain = self.blockchain.chain[:]
+            potential_chain.append(block)
 
+            try:
+                self.blockchain.replace_chain(potential_chain)
+                print('\n -- Successfully replaced the local chain')
+            except Exception as e:
+                print(f'\n -- Did not replace chain: {e}')
 
 class PubSub():
     """
     Handles the publish/subscribe layer of the application.
     Provides communication between the nodes of the blockchain network.
     """
-    def __init__(self):
+    def __init__(self, blockchain):
         self.pubnub = PubNub(pnconfig)
-        self.pubnub.subscribe().channels([TEST_CHANNEL]).execute()
-        self.pubnub.add_listener(Listener())
-    
+        self.pubnub.subscribe().channels(CHANNELS.values()).execute()
+        self.pubnub.add_listener(Listener(blockchain))
+
     def publish(self, channel, message):
         """
         Publish the message object to the channel.
         """
         self.pubnub.publish().channel(channel).message(message).sync()
 
+    def broadcast_block(self, block):
+        """
+        Broadcast a block object to all nodes.
+        """
+        self.publish(CHANNELS['BLOCK'], block.to_json())
 
 def main():
     pubsub = PubSub()
+
     time.sleep(1)
-    pubsub.publish(TEST_CHANNEL, {'foo': 'bar'})
+
+    pubsub.publish(CHANNELS['TEST'], { 'foo': 'bar' })
 
 if __name__ == '__main__':
     main()
